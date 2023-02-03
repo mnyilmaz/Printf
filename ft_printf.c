bcee1afca53bb67b94c8b62d840a7c2e0a97ab2a
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-03 23:40:54 +0300

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 17:52:14 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/02/03 17:51:30 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/02/03 19:13:28 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,31 @@
 
 int ft_printf(const char *fmt, ...)
 {
-	t_print print;
 	va_list to_do;
+	char spec;
+	int len;
+
+	len = 0;
 	va_start(to_do, fmt);
 	int i = 0;
-	if(!*fmt)
-		return (write(1, "(null)", 6));
-	while(*(fmt + i))
+	while (*(fmt + i))
 	{
 		if(*(fmt + i) == '%')
 		{
 			i++;
-			print.specifier = *(fmt + i++);
-			ft_states(print, to_do);
+			spec = *(fmt + i);
+			len += ft_states(spec, to_do);
 		}
 		else
-			ft_print_char(*(fmt + i));
+			len += ft_print_char(*(fmt + i));
 		i++;
 	}
-	return (i);
+	va_end(to_do);
+	return (len);
 }
 
 #include <stdio.h>
 int main(void)
 {
-	int a = 6;
-	int *ptr;
-	
-	ptr = &a;
-	printf("ORG: merhaba %p\n", ptr);
-	ft_printf("MY: merhaba %p", ptr);
-	printf("\n");
-	ft_printf("MY: merhaba %x", 99);
+	printf("%d\n", ft_printf("%d", 5));
 }
