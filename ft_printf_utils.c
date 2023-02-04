d749ec6b740db383467960e8ac165a559a0c6e59
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-04 15:29:09 +0300

    Update ft_printf_utils.c

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,36 +6,34 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 18:05:05 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/02/03 18:56:52 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/02/04 15:28:30 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_states(va_list to_do, t_printf print)
+int	ft_states(char spec, va_list to_do)
 {
-	if (print.spec == 'c')
-		print.len += ft_print_char(va_arg(to_do, int));
-	else if (print.spec == 's')
-		print.len += ft_print_str(va_arg(to_do, char*));
-	else if (print.spec == 'p')
+	if (spec == 'c')
+		return (ft_print_char(va_arg(to_do, int)));
+	else if (spec == 's')
+		return( ft_print_str(va_arg(to_do, char*)));
+	else if (spec == 'p')
 	{
 		write(1, "0x", 2);
-		print.len += ft_print_rest(va_arg(to_do, unsigned long), 16, BASE, P1) + 2; //64 bit garantilemesi
+		return (ft_print_rest(va_arg(to_do, unsigned long), H_LEN, BASE) + 2); 
 	}
-	else if (print.spec == 'i')
-		print.len += ft_print_rest(va_arg(to_do, int), 10, DEC, P2); 
-	else if (print.spec == 'u')
-		print.len += ft_print_rest(va_arg(to_do, unsigned int), 10, DEC, P2); 
-	else if (print.spec == 'd')
-		print.len += ft_print_rest(va_arg(to_do, unsigned int), 10, DEC, P2); 
-	else if (print.spec == 'x')
-		print.len += ft_print_rest(va_arg(to_do, unsigned int), 16, BASE, P2);
-	else if (print.spec == 'X')
-		print.len += ft_print_rest(va_arg(to_do, unsigned int), 16, BASE_U, P2);
-	else if (print.spec == '%')
-		print.len += ft_print_char('%');
-	return (print.len);
+	else if (spec == 'i' || spec == 'd')
+		return (ft_print_int(va_arg(to_do, int))); 
+	else if (spec == 'u')
+		return (ft_print_rest(va_arg(to_do, unsigned int), D_LEN, DEC)); 
+	else if (spec == 'x')
+		return (ft_print_rest(va_arg(to_do, unsigned int), H_LEN, BASE));
+	else if (spec == 'X')
+		return (ft_print_rest(va_arg(to_do, unsigned int), H_LEN, BASE_U));
+	else if (spec == '%')
+		return (ft_print_char('%'));
+	return (0);
 }
 
 int	ft_print_char(char c)
@@ -43,45 +41,61 @@ int	ft_print_char(char c)
 	return (write(1, &c, 1));
 }
 
+// int	ft_strlen(char *s)
+// {
+// 	if (!*s)
+// 		return (0);
+// 	return (1 + ft_strlen(s + 1));
+// }
+
 int	ft_print_str(char *s)
 {
-	int slen;
-
-	slen = 0;
+	int len;
+	
+	len = 0;
 	if (!s)
 		return (ft_print_str("(null)"));
 	while (*s)
 	{
 		ft_print_char(*s++);
-		slen++;
+		len++;
 	}
-	return (slen);
+	return (len);
 }
 
-int	ft_print_rest(long long int num, int len, char *base, int point)
+int    ft_print_rest(size_t num, size_t base_len, char *base)
 {
 	int	rlen;
 
 	rlen = 0;
-	if (num < 0)
-	{
-		rlen += ft_print_char('-');
-		num *= -1;
-	}
-	if (num >= len)
-	{
-		if (point == 1)
-		{
-			rlen += ft_print_rest((num / len), len, base, point);
-			rlen += ft_print_rest((num % len), len, base, point);
-		}
-		else if (point == 2)
-		{
-			rlen += ft_print_rest((num / len), len, base, point);
-			rlen += ft_print_rest((num % len), len, base, point);
-		}
+    if (num >= base_len)
+    {
+        rlen += ft_print_rest(num / base_len, base_len, base);
+        rlen += ft_print_rest(num % base_len, base_len, base);
+    }
+	else
+		rlen += ft_print_char(base[num]);	
+	return (rlen);
+}
+
+int	ft_print_int(int num)
+{
+	int	rlen;
+
+	rlen = 0;
+	if (num == -__INT_MAX__ - 1)
+		return (ft_print_str("-2147483648"));
+    if (num < 0)
+    {
+        rlen += ft_print_char('-');
+        rlen += ft_print_int(num * -1);
+    }
+    else if (num >= 10)
+    {
+        rlen += ft_print_int(num / 10);
+        rlen += ft_print_int(num % 10);
 	}
 	else
-		rlen += ft_print_char(base[num]);
+		rlen += ft_print_char(num + '0');
 	return (rlen);
 }
