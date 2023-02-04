ae35b0bb7470d6615f41d83f87f65c022e1d34e3
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-04 15:30:33 +0300

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 17:52:14 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/02/03 19:13:28 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/02/04 15:29:59 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 int ft_printf(const char *fmt, ...)
 {
-	t_printf print;
 	va_list to_do;
-	//char spec;
-	//int len;
+	char spec;
+	int len;
+	int result;
 
-	//len = 0;
+	result = 0;
+	len = 0;
 	va_start(to_do, fmt);
 	int i = 0;
 	while (*(fmt + i))
@@ -27,24 +28,23 @@ int ft_printf(const char *fmt, ...)
 		if(*(fmt + i) == '%')
 		{
 			i++;
-			print.spec = *(fmt + i);
-			print.result += ft_states(to_do, print);
+			spec = *(fmt + i);
+			len += ft_states(spec, to_do);
 		}
 		else
-			print.result += ft_print_char(*(fmt + i));
+			len += ft_print_char(*(fmt + i));
 		i++;
 	}
 	va_end(to_do);
-	return (print.result);
+	return (len);
 }
-
+#include <limits.h>
 #include <stdio.h>
 int main(void)
 {
-	int a = 5;
-	int *ptr;
-	ptr = &a;
-
+	int	a = 5;
+	int	*ptr = &a;
+	printf("%d\n", ft_printf( " %d ", -1));
 	ft_printf("");
 	printf("\n");
 	ft_printf("extraordinary");
@@ -68,8 +68,9 @@ int main(void)
 	printf("\n");
 	ft_printf("%%");
 	printf("\n");
+	printf("\n");
 
-	ft_printf("%d", ft_printf("extraordinary"));
+	ft_printf(": %d", ft_printf("extraordinary"));
 	printf("\n");
 	ft_printf("%d", ft_printf("%c: ", 's'));
 	printf("\n");
@@ -85,8 +86,9 @@ int main(void)
 	printf("\n");
 	ft_printf("%d", ft_printf("%%: "));
 	printf("\n");
+	printf("\n");
 
-	printf("%d\n", ft_printf("extraordinary"));
+	printf(": %d\n", ft_printf("extraordinary"));
 	printf("%d\n", ft_printf("%c: ", 's'));
 	printf("%d\n", ft_printf("%s: ", "extiraordinary"));
 	printf("%d\n", ft_printf("%p: ", ptr));
